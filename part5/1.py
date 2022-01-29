a43d1209bc38da2e60c49968ab75bcc0ca77d757
olegthundr <oksidox@gmail.com>
2022-01-29 20:31:59 +0900

    add some test files

---
+++ part5/1.py
@@ -0,0 +1,3 @@
+# Write your code here :-)
+protocol = input('Which your protocol is the most favourite? ')
+print(protocol)
---
+++ part5/2.py
@@ -0,0 +1,5 @@
+# Write your code here :-)
+value = '11111111111111111111111111110000'
+oct1, oct2, oct3, oct4 = value.split(maxsplit=4)
+print(oct2)
+#print(int(value[0:7]))
